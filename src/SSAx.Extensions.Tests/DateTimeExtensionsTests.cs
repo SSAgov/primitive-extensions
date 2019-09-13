85b5041c9fbe1cabf597f0e78707d9a5a7a7750a
Amalfitano, Joseph <Joseph.Amalfitano@ssa.gov>
2019-09-13 14:52:09 -0400

    added DateTime extensions

---
+++ src/SSAx.Extensions.Tests/DateTimeExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SSAx.Extensions.Tests
+{
+    public class DateTimeExtensionsTests
+    {
+        [Fact]
+        public void EncodeAsSqlVariable_GivenDate_PrecisionToYear_ExpectMatch()
+        {
+            DateTime d = new DateTime(2003, 4, 17, 8, 37, 12, 123);
+            string result = d.EncodeAsSqlVariable(DateTimeExtensions.DateTimePrecision.Year);
+            Assert.Equal("'2003-01-01'", result);
+        }
+
+        [Fact]
+        public void EncodeAsSqlVariable_GivenDate_PrecisionToMonth_ExpectMatch()
+        {
+            DateTime d = new DateTime(2003, 4, 17, 8, 37, 12, 123);
+            string result = d.EncodeAsSqlVariable(DateTimeExtensions.DateTimePrecision.Month);
+            Assert.Equal("'2003-04-01'", result);
+        }
+
+
+        [Fact]
+        public void EncodeAsSqlVariable_GivenDate_PrecisionToDay_ExpectMatch()
+        {
+            DateTime d = new DateTime(2003, 4, 17, 8, 37, 12, 123);
+            string result = d.EncodeAsSqlVariable(DateTimeExtensions.DateTimePrecision.Day);
+            Assert.Equal("'2003-04-17'", result);
+        }
+
+        [Fact]
+        public void EncodeAsSqlVariable_GivenDate_PrecisionToHour_ExpectMatch()
+        {
+            DateTime d = new DateTime(2003, 4, 17, 8, 37, 12, 123);
+            string result = d.EncodeAsSqlVariable(DateTimeExtensions.DateTimePrecision.Hour);
+            Assert.Equal("'2003-04-17 08:00:00'", result);
+        }
+
+        [Fact]
+        public void EncodeAsSqlVariable_GivenDate_PrecisionToMinute_ExpectMatch()
+        {
+            DateTime d = new DateTime(2003, 4, 17, 8, 37, 12, 123);
+            string result = d.EncodeAsSqlVariable(DateTimeExtensions.DateTimePrecision.Minute);
+            Assert.Equal("'2003-04-17 08:37:00'", result);
+        }
+
+
+
+        [Fact]
+        public void EncodeAsSqlVariable_GivenDate_PrecisionToSecond_ExpectMatch()
+        {
+            DateTime d = new DateTime(2003, 4, 17, 8, 37, 12, 123);
+            string result = d.EncodeAsSqlVariable(DateTimeExtensions.DateTimePrecision.Second);
+            Assert.Equal("'2003-04-17 08:37:12'", result);
+        }
+
+        [Fact]
+        public void EncodeAsSqlVariable_GivenDate_PrecisionToMilliSecond_ExpectMatch()
+        {
+            DateTime d = new DateTime(2003, 4, 17, 8, 37, 12, 123);
+            string result = d.EncodeAsSqlVariable(DateTimeExtensions.DateTimePrecision.Millisecond);
+            Assert.Equal("'2003-04-17 08:37:12.123'", result);
+        }
+    }
+}
---
+++ src/SSAx.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace SSAx.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Returns date value in a string format at a specified precision
+        /// </summary>
+        /// <param name="datetime"></param>
+        /// <param name="precision"></param>
+        /// <returns></returns>
+        public static string EncodeAsSqlVariable(this DateTime datetime, DateTimePrecision precision)
+        {
+            string encodedString = "";
+            string format = "";
+            switch (precision)
+            {
+                
+                case DateTimePrecision.Year:
+                    format = "yyyy-MM-dd";
+                    datetime = new DateTime(datetime.Year, 1, 1);
+
+                    break;
+                case DateTimePrecision.Month:
+                    format = "yyyy-MM-dd";
+                    datetime = new DateTime(datetime.Year, datetime.Month, 1);
+                    break;
+
+                case DateTimePrecision.Day:
+                    format = "yyyy-MM-dd";
+                    break;
+
+                case DateTimePrecision.Hour:
+                    format = "yyyy-MM-dd HH:mm:ss";
+                    datetime = new DateTime(datetime.Year, datetime.Month, datetime.Day,datetime.Hour,0,0);
+                    break;
+
+                case DateTimePrecision.Minute:
+                    format = "yyyy-MM-dd HH:mm:ss";
+                    datetime = new DateTime(datetime.Year, datetime.Month, datetime.Day, datetime.Hour, datetime.Minute, 0);
+                    break;
+
+                case DateTimePrecision.Second:
+                    format = "yyyy-MM-dd HH:mm:ss";
+                    break;
+
+                case DateTimePrecision.Millisecond:
+                    format = "yyyy-MM-dd HH:mm:ss.fff";
+                    break;
+
+                default:
+                    break;
+            }
+            encodedString = string.Format($"'{datetime.ToString(format)}'");
+            return encodedString;
+        }
+
+        public enum DateTimePrecision{
+            Year,
+            Month,
+            Day,
+            Hour,
+            Minute,
+            Second,
+            Millisecond
+        }
+    }
+}
