08f568741727c261e6dad80ad55168a44ad55442
Amalfitano, Joseph <Joseph.Amalfitano@ssa.gov>
2019-09-13 14:31:00 -0400

    added boolean extensions

---
+++ src/SSAx.Extensions.Tests/BooleanExtensionsTests.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using Xunit;
+
+namespace SSAx.Extensions.Tests
+{
+    public class BooleanExtensionsTests
+    {
+
+        [Fact]
+        public void ToIndicatingStrings_GivenNull_ExpectNull()
+        {
+             
+            bool? b = null;
+            string s = b.ToIndicatingStrings("yes", "no");
+            Assert.Null(s);
+        }
+        [Fact]
+        public void ToIndicatingStrings_GivenTrue_Expectyes()
+        {
+
+            bool? b = true;
+            string s = b.ToIndicatingStrings("yes", "no");
+            Assert.Equal("yes", s);
+        }
+
+        [Fact]
+        public void ToIndicatingStrings_GivenFalse_ExpectNo()
+        {
+
+            bool? b = false;
+            string s = b.ToIndicatingStrings("yes", "no");
+            Assert.Equal("no", s);
+        }
+
+
+
+        [Fact]
+        public void ToBoolNullable_GivenNull_ExpectNull()
+        {
+            string s = null; 
+            bool? result = s.ToBoolNullable();
+            Assert.Null(result);
+        }
+
+
+        [Fact]
+        public void ToBoolNullable_GivenEmptyString_ExpectNull()
+        {
+            string s = "";
+            bool? result = s.ToBoolNullable();
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void ToBoolNullable_GivenWhitespace_ExpectNull()
+        {
+            string s = "     ";
+            bool? result = s.ToBoolNullable();
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void ToBoolNullable_true_ExpectTrue()
+        {
+            string s = "true";
+            bool? result = s.ToBoolNullable();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ToBool_GivenY_ExptectTrue()
+        {
+            string s = "Y";
+            bool result = s.ToBool();
+            Assert.True(result);
+        }    
+
+        [Fact]
+        public void ToBool_GivenYeah_ExptectTrue()
+        {
+            string s = "Yeah";
+            bool result = s.ToBool();
+            Assert.True(result);
+        }
+
+
+        [Fact]
+        public void ToBool_GivenTRUE_ExptectTrue()
+        {
+            string s = "TRUE";
+            bool result = s.ToBool();
+            Assert.True(result);
+        }
+
+
+        [Fact]
+        public void ToBool_GivenFALSE_ExptectFalse()
+        {
+            string s = "FALSE";
+            bool result = s.ToBool();
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ToBool_GivenEmptyString_ExptectException()
+        {
+            string s = "";
+            Assert.Throws<Exception>(()=> s.ToBool());
+        }
+
+        [Fact]
+        public void ToBool_GivenNullString_ExpectException()
+        {
+            string s = null;
+            Assert.Throws<ArgumentNullException>(() => s.ToBool());
+        }
+        [Fact]
+        public void ToBool_GivenYes_ExptectTrue()
+        {
+            string s = "Yes";
+            bool result = s.ToBool();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ToBool_Givenyes_ExptectTrue()
+        {
+            string s = "yes";
+            bool result = s.ToBool();
+            Assert.True(result);
+        }
+        [Fact]
+        public void ToBool_Given1_ExptectTrue()
+        {
+            string s = "1";
+            bool result = s.ToBool();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ToBool_Given100string_ExptectTrue()
+        {
+            string s = "100";
+            bool result = s.ToBool();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ToBool_Given10d0string_ExptectTrue()
+        {
+            string s = "10.0";
+            bool result = s.ToBool();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ToBool_Given0string_ExptectFalse()
+        {
+            string s = "0";
+            bool result = s.ToBool();
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ToBool_Given000string_ExptectFalse()
+        {
+            string s = "000";
+            bool result = s.ToBool();
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ToBool_Given000d_ExptectFalse()
+        {
+            decimal d = 000;
+            bool result = d.ToBool();
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ToBool_Given000integer_ExptectFalse()
+        {
+            int i = 000;
+            bool result = i.ToBool();
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ToBool_Given01decimal_ExptectTrue()
+        {
+            decimal d = 0.1m;
+            bool result = d.ToBool();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ToSwitch_GivenTrue_ExpectYes()
+        {
+            string response = null;
+            bool? b = true;
+            response = b.ToSwitch("Yes", "No");
+            Assert.Equal("Yes", response);
+        }
+        [Fact]
+        public void ToSwitch_GivenFalse_ExpectNo()
+        {
+            string response = null;
+            bool? b = false;
+            response = b.ToSwitch("Yes", "No");
+            Assert.Equal("No", response);
+        }
+        [Fact]
+        public void ToSwitch_GivenNull_ExpectNull()
+        {
+            string response = null;
+            bool? b = null;
+            response = b.ToSwitch("Yes", "No");
+            Assert.Null(response);
+        }
+    }
+}
---
+++ src/SSAx.Extensions/BooleanExtensions.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+
+namespace SSAx.Extensions
+{
+    public static class BooleanExtensions
+    {
+        /// <summary>
+        /// This method can be called on a boolean type object
+        /// It returns value associated with true and false based on boolean state
+        /// </summary>
+        /// <param name="b"></param>
+        /// <param name="trueValue"></param>
+        /// <param name="falseValue"></param>
+        /// <param name="nullValue"></param>
+        /// <returns></returns>
+        public static string ToIndicatingStrings(this bool? b, string trueValue, string falseValue = "", string nullValue = null)
+        {
+            if (b == null)
+                return nullValue;
+            else
+                return b.Value.ToIndicatingStrings(trueValue, falseValue);
+        }
+        public static string ToSwitch(this bool? b, string trueValue = "Y", string falseValue= "N")
+        {
+            return b.ToIndicatingStrings(trueValue, falseValue);
+        }
+
+        /// <summary>
+        /// Converts a boolean to a string. Zero returns false, any non-zero returns true.
+        /// </summary>
+        /// <param name="b"></param>
+        /// <param name="trueValue"></param>
+        /// <param name="falseValue"></param>
+        /// <returns></returns>
+        public static string ToIndicatingStrings(this bool b, string trueValue, string falseValue="")
+        {
+            return b ? trueValue : falseValue;
+        }
+        /// <summary>
+        /// Converts an unsigned integer to a boolean. Zero returns false, any non-zero returns true.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public static bool ToBool(this int i)
+        {
+            return i != 0;
+        }
+        /// <summary>
+        /// Converts a decimal to a boolean. Zero returns false, any non-zero returns true.
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public static bool ToBool(this decimal d)
+        {
+            return d != 0;
+        }
+
+        /// <summary>
+        /// Converts a string to a nullable boolean. Null, whitespace, and empty returns null.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool? ToBoolNullable(this string s)
+        {
+            if (s.IsNullOrEmptyString() || s == string.Empty|| string.IsNullOrWhiteSpace(s))
+            {
+                return null;
+            }
+            return s.ToBool();
+        }
+        
+        /// <summary>
+        /// Converts a string to a boolean. Zero returns false, any non-zero returns true.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool ToBool(this string s)
+        {
+            if (s == null)
+                throw new ArgumentNullException();
+
+            if (s.Length == 0 )
+            {
+                throw new Exception("Empty string not allowed");
+            }
+            string[] trueStrings = { "Y", "1", "-1", "TRUE", "YES", "T", "POSITIVE" };
+            string[] falseStrings = { "N", "0", "FALSE", "F", "NO", "NEGATIVE" };
+
+            if (trueStrings.Contains(s.ToUpper()))
+            {
+                return true;
+            }
+            if (falseStrings.Contains(s.ToUpper()))
+            {
+                return false;
+            }
+            char firstChar = s.ToUpper()[0];
+            if (firstChar == 'Y')
+            {
+                return true;
+            }
+            if (firstChar == 'N')
+            {
+                return false;
+            }
+
+            if (s.IsANumber())
+            {
+                if (s.IsAWholeNumber())
+                {
+                    return s.NzInt().ToBool();
+                }
+                if(s.IsADecimal())
+                {
+                    return s.NzDecimal().ToBool();
+                }
+            }
+
+            throw new Exception("No viable match");
+        }
+    }
+}
