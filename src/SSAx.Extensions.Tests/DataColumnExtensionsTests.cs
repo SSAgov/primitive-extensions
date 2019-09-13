97fa45111e1c7219661226698424b950b748fec3
Amalfitano, Joseph <Joseph.Amalfitano@ssa.gov>
2019-09-13 09:03:56 -0400

    added DataColumn extensions & tests

---
+++ src/SSAx.Extensions.Tests/DataColumnExtensionsTests.cs
@@ -0,0 +1,87 @@
+﻿using System.Data;
+using Xunit;
+
+namespace SSAx.Extensions.Tests
+{
+    public class DataColumnExtensionsTests
+    {
+        [Fact]
+        public void IsAPrimaryKeyColumn_GivenTableWithSingleColumnIntKey_ExpectTrue()
+        {
+            DataTable dtA = new DataTable();
+            DataColumn c = new DataColumn("Id", typeof(int));
+            dtA.Columns.Add(c);
+            dtA.Columns.Add("First Name");
+            dtA.Columns.Add("Last Name");
+            dtA.PrimaryKey = new DataColumn[] { c };
+
+            bool result = c.IsAPrimaryKeyColumn(dtA);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsAPrimaryKeyColumn_GiveNonPrimaryKey_ExpectFalse()
+        {
+            DataTable dtA = new DataTable();
+            DataColumn a = new DataColumn("First Name");
+            DataColumn b = new DataColumn("Last Name");
+            DataColumn c = new DataColumn("Id", typeof(int));
+            dtA.Columns.Add(c);
+            dtA.Columns.Add(b);
+            dtA.Columns.Add(a);
+            dtA.PrimaryKey = new DataColumn[] { c };
+
+            bool result = a.IsAPrimaryKeyColumn(dtA);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsAPrimaryKeyColumnKey_GivenColwithNonKey_ExpectFalse()
+        {
+            DataTable dtA = new DataTable();
+            DataColumn c = new DataColumn("Id", typeof(int));
+            dtA.Columns.Add("Id", typeof(int));
+            dtA.Columns.Add("First Name");
+            dtA.Columns.Add("Last Name");
+
+            Assert.False(c.IsAPrimaryKeyColumn(dtA));
+        }
+
+        [Fact]
+        public void IsInUniqueConstraint_GivenConstrainColumn_ExpectTrue()
+        {
+            DataTable dtA = new DataTable();
+            DataColumn colId = new DataColumn("Id", typeof(int));
+            DataColumn colLastName = new DataColumn("LastName",typeof(string));
+            dtA.Columns.Add(colId);
+            dtA.Columns.Add("First Name");
+            dtA.Columns.Add(colLastName);
+            dtA.PrimaryKey = new DataColumn[] { colId };
+
+            dtA.Constraints.Add("alternateKey_names", new DataColumn[] { colLastName }, false);
+
+            
+
+            Assert.True(colLastName.IsInAUniqueConstraint(dtA));
+        }
+
+        [Fact]
+        public void IsInUniqueConstraint_GivenNonConstraintColumn_ExpectFalse()
+        {
+            DataTable dtA = new DataTable();
+            DataColumn colId = new DataColumn("Id", typeof(int));
+            DataColumn colLastName = new DataColumn("LastName", typeof(string));
+            DataColumn colFirstName = new DataColumn("FirstName", typeof(string));
+            dtA.Columns.Add(colId);
+            dtA.Columns.Add(colFirstName);
+            dtA.Columns.Add(colLastName);
+            dtA.PrimaryKey = new DataColumn[] { colId };
+
+            dtA.Constraints.Add("alternateKey_names", new DataColumn[] { colLastName }, false);
+
+            Assert.False(colFirstName.IsInAUniqueConstraint(dtA));
+        }
+    }
+}
---
+++ src/SSAx.Extensions/DataColumnExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+
+namespace SSAx.Extensions
+{
+    public static class DbColumnExtensions
+    {
+        /// <summary>
+        /// Returns true if the column is primary key column
+        /// </summary>
+        /// <param name="c"></param>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static bool IsAPrimaryKeyColumn(this DataColumn c, DataTable dt)
+        {
+            if (dt.PrimaryKey != null)
+            {
+                foreach (DataColumn pk in dt.PrimaryKey)
+                {
+                    if (pk.ColumnName == c.ColumnName)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// Returns true if the column has unique constraint
+        /// </summary>
+        /// <param name="c"></param>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static bool IsInAUniqueConstraint(this DataColumn c, DataTable dt)
+        {
+            foreach (UniqueConstraint con in dt.Constraints)
+            {
+                foreach (DataColumn akColumn in con.Columns)
+                {
+                    if (akColumn.ColumnName == c.ColumnName)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
