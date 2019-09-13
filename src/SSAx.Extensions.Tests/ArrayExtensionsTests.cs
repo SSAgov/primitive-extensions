32c677195262794035abc9a46c55c51cf077cc6e
Amalfitano, Joseph <Joseph.Amalfitano@ssa.gov>
2019-09-13 08:57:29 -0400

    added array extensions

---
+++ src/SSAx.Extensions.Tests/ArrayExtensionsTests.cs
@@ -0,0 +1,99 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace SSAx.Extensions.Tests
+{
+    public class ArrayExtensionsTests
+    {
+        [Fact]
+        public void CreateSubsets_Given_0_nodes_expect_0_subsets()
+        {
+            string[] words = { };
+
+            var subsets = words.CreateSubsets();
+            Assert.Empty(subsets);
+        }
+
+        [Fact]
+        public void CreateSubsets_Given_1_nodes_expect_1_subsets()
+        {
+            string s = "Hello";
+            string[] words = s.Split(' ');
+
+            var subsets = words.CreateSubsets();
+            Assert.Single(subsets);
+
+            Assert.Contains(new string[] { "Hello" }, subsets);
+        }
+
+        [Fact]
+        public void CreateSubsets_Given_2_nodes_expect_3_subsets()
+        {
+            string s = "Hello there,";
+            string[] words = s.Split(' ');
+
+            var subsets = words.CreateSubsets();
+            Assert.Equal(3,subsets.Count());
+            Assert.Contains(new string[] { "Hello" }, subsets);
+            Assert.Contains(new string[] { "there," }, subsets);
+            Assert.Contains(new string[] { "Hello","there," }, subsets);
+
+        }
+
+        [Fact]
+        public void CreateSubsets_Given_3_nodes_expect_7_subsets()
+        {
+            string s = "Hello there, how";
+            string[] words = s.Split(' ');
+
+            var subsets = words.CreateSubsets();
+            Assert.Equal(7, subsets.Count());
+            Assert.Contains(new string[] { "Hello" }, subsets);
+            Assert.Contains(new string[] { "there," }, subsets);
+            Assert.Contains(new string[] { "how" }, subsets);
+            Assert.Contains(new string[] { "Hello", "there," }, subsets);
+            Assert.Contains(new string[] { "there,", "how"}, subsets);
+            Assert.Contains(new string[] { "Hello", "how" }, subsets);
+            Assert.Contains(new string[] { "Hello", "there,", "how" }, subsets);
+        }
+
+        [Fact]
+        public void CreateSubsets_Given_4_nodes_expect_15_subsets()
+        {
+            string s = "Hello there, how are";
+            string[] words = s.Split(' ');
+
+            var subsets = words.CreateSubsets();
+            Assert.Equal(15, subsets.Count());
+            Assert.Contains(new string[] { "Hello" }, subsets); // 1
+            Assert.Contains(new string[] { "there," }, subsets);  // 2
+            Assert.Contains(new string[] { "how" }, subsets); // 3
+            Assert.Contains(new string[] { "are" }, subsets); // 4
+            Assert.Contains(new string[] { "Hello", "there," }, subsets); // 5
+            Assert.Contains(new string[] { "Hello", "how" }, subsets); // 6
+            Assert.Contains(new string[] { "Hello", "are" }, subsets); // 7
+            Assert.Contains(new string[] { "there,", "how" }, subsets); // 8
+            Assert.Contains(new string[] { "there,", "are" }, subsets); // 9
+            Assert.Contains(new string[] { "how,", "are" }, subsets); // 10
+            Assert.Contains(new string[] { "Hello", "there,", "how" }, subsets); // 11
+            Assert.Contains(new string[] { "Hello", "there,", "are" }, subsets); // 12
+            Assert.Contains(new string[] { "Hello", "how", "are" }, subsets); // 13
+            Assert.Contains(new string[] { "there,", "how", "are" }, subsets); // 14
+            Assert.Contains(new string[] { "Hello", "there,", "how", "are" }, subsets); //15
+
+        }
+
+        [Fact]
+        public void CreateSubsets_Given_5_nodes_expect_31_subsets()
+        {
+            string s = "Hello there, how are you?";
+            string[] words = s.Split(' ');
+
+            var subsets = words.CreateSubsets();
+            Assert.Equal(31, subsets.Count);
+        }
+
+
+    }
+}
+
---
+++ src/SSAx.Extensions/ArrayExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+
+namespace SSAx.Extensions
+{
+
+    public static class ArrayExtensions
+    {
+        //The following code is drawn from a posting by Anthony Pegram (https://stackoverflow.com/users/414076/anthony-pegram) 
+        //posted to Stack Overflow (https://stackoverflow.com/questions/3319586/getting-all-possible-combinations-from-a-list-of-numbers/3319689). 
+        //Per Stack Exchange’s Terms of Service, all user contributions to Stack Overflow are licensed under the Creative Commons Attribution ShareAlike 3.0 License (CC-BY-SA 3.0), available at https://creativecommons.org/licenses/by-sa/3.0/legalcode .”
+        /// <summary>
+        /// This method can be called on an array.
+        /// It returns subset of an array as a list of array. (for {a,b} it returns {a,b},{a},{b})
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="originalArray"></param>
+        /// <returns></returns>
+        public static List<T[]> CreateSubsets<T>(this T[] originalArray)
+        {
+            List<T[]> subsets = new List<T[]>();
+
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                int subsetCount = subsets.Count;
+                subsets.Add(new T[] { originalArray[i] });
+
+                for (int j = 0; j < subsetCount; j++)
+                {
+                    T[] newSubset = new T[subsets[j].Length + 1];
+                    subsets[j].CopyTo(newSubset, 0);
+                    newSubset[newSubset.Length - 1] = originalArray[i];
+                    subsets.Add(newSubset);
+                }
+            }
+            
+            return subsets;
+        }
+    }
+}
